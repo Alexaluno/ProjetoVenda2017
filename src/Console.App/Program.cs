a5399a606623fd5a2fe6d7b39b7448ba6ad41663
Rodrigo <derodrigo@brturbo.com.br>
2017-12-22 20:37:39 -0200

    Iniciado a digitação do codigo

--- src/Console.App/Program.cs
+++ src/Console.App/Program.cs
@@ -7,6 +7,7 @@ using Util.Entidades.Usuarios.Repositorio;
 using Util.Entidades.Vendas.Repositorio;
 using Util.Entidades.Vendas;
 using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp
 {
@@ -14,13 +15,68 @@ namespace ConsoleApp
     {
         public static void Main()
         {
+            //Codigo Usuario
+            var usuario1 = new Usuario("Rodrigo", true);
+            var usuario2 = new Usuario("Alex", false); 
+            var usuarioRepositorio = new UsuarioRepositorio();
+            usuarioRepositorio.Usuarios.Add(usuario1);
+            usuarioRepositorio.Usuarios.Add(usuario2);
 
-         
-            //cria um cliente
+            var retornoUsuarioRepositorio = usuarioRepositorio.Usuarios;
 
-            //add um cliente no repositorio
+            foreach (var item in retornoUsuarioRepositorio)
+            {
+                Console.WriteLine("Nome :" + item.Nome + " | " + "Administrador: " + item.Administrador);
+            }
+            Console.ReadLine();
 
-            //....
+            //limpa a tela
+            Console.Clear();
+
+            //Codigo Cliente
+            var cliente1 = new Cliente("Juliana", "33333333333", "Taquara");
+            var cliente2 = new Cliente("Daniela", "22222222222", "Taquara");
+            var clienteRepositorio = new ClienteRepositorio();
+            clienteRepositorio.Clientes.Add(cliente1);
+            clienteRepositorio.Clientes.Add(cliente2);
+
+            var retornoClienteRepositorio = clienteRepositorio.Clientes;
+
+            foreach (var item in retornoClienteRepositorio)
+            {
+                Console.WriteLine("Nome: " + item.Nome + " | " + "CPF: " + item.Cpf + " | " + "Cidade: " + item.Endereco);
+            }
+            Console.ReadLine();
+
+            //limpa a tela
+            Console.Clear();
+
+            //Codigo Produto
+            var produto1 = new Produto("Cerveja", Convert.ToDecimal(2.49));
+            var produto2 = new Produto("Refri", Convert.ToDecimal(1.19));
+            var produtoRepositorio = new ProdutoRepositorio();
+            produtoRepositorio.Produtos.Add(produto1);
+            produtoRepositorio.Produtos.Add(produto2);
+
+            var retornoProdutoRepositorio = produtoRepositorio.Produtos;
+
+            foreach (var item in retornoProdutoRepositorio)
+            {
+                Console.WriteLine("Produto: " + item.Descricao + " | " + "Valor: R$" + item.Valor);
+            }
+            Console.ReadLine();
+
+            //Codigo Venda
+            foreach (var user in retornoUsuarioRepositorio)
+            {
+                foreach (var client in retornoClienteRepositorio)
+                {
+                    var venda = new Venda("001", user, client, retornoProdutoRepositorio, 200, 20);
+                    Console.WriteLine("Num.Ped: " + venda.Pedido + " | " + "Usuario: " + venda.Usuario.Nome + " | " + "Cliente: " + venda.Cliente.Nome + " | " + "Produto(s): " + venda.Produtos + " | " + "Valor Total: R$" + venda.ValorTotal + " | " + "Quantidade Total: " + venda.QuantidadeTotal);
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadLine();
 
         }
     }
